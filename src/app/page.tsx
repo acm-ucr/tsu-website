5ece8952e9e8e9ffa08092df4f06eb0aea23c494
shuuvi <janeleebmes@gmail.com>
2025-06-25 16:49:49 -0600

    added the public,fonts,colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center text-5xl font-tsu-sahitya">
       Hello World
     </div>
   );
