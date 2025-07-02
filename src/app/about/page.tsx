a0ee2c5d62adc372c9f28ebb3826eb43115387db
cashums <136028501+cashums@users.noreply.github.com>
2025-07-02 18:50:29 +0900

    Added Figma design elements (font, spacing, line height, color) to a <p> element for the About Us Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
-const Home = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      About
-    </div>
-  );
-};
-
-export default Home;
+const About = () => {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <p style={{fontFamily: "var(--font-tsu-sahitya)"}} className="font-tsu-sahitya font-normal text-[96px] leading-[80px] text-black">About Us</p>
+      </div>
+    );
+  };
+  
+  export default About;
