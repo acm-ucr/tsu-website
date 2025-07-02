520a6f3c4ede23fa2779bb0564bfcb84bf5a631e
Yubin Zhen <yubinzhen020105@gmail.com>
2025-07-02 01:09:34 -0700

    fixed comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import placeholder from "@/public/placeholder.webp";
 
 const Home = () => {
   return (
-    <div className="bg-tsu-blue-100 flex h-screen w-screen">
+    <div className="bg-tsu-blue-100 flex h-auto w-screen">
       <Image
         src={placeholder}
         alt="Placeholder Image"
-        className="ml-30 h-[325px] w-[440px] p-13"
+        className="ml-17 w-1/3 p-13"
       />
 
-      <div className="ml-40 flex w-5/10 flex-col text-white">
-        <div className="font-tsu-sahitya mt-17 mb-4 text-center text-5xl text-white">
+      <div className="font-tsu-sahitya mt-8 ml-25 flex w-5/10 flex-col text-white">
+        <div className="mt-17 mb-4 text-center text-5xl text-white">
           <span className="text-tsu-pink-100 mr-2">|</span>Who Are We?
         </div>
-        <p className="text-xl text-white">
+        <p className="text-2xl text-white">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi Lorem
