6b6bf8b895a4da8395c0a493b754dbf24929e224
cashums <136028501+cashums@users.noreply.github.com>
2025-07-03 05:54:16 +0900

    Adjusted font class

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,18 @@
 const About = () => {
-    return (
-      <div className="flex h-screen w-screen items-center justify-center">
-        <p style={{fontFamily: "var(--font-tsu-sahitya)", width: "363px", height: "91px",}} className="font-tsu-sahitya font-normal text-[96px] leading-[80px] text-black">About Us</p>
-      </div>
-    );
-  };
-  
-  export default About;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <p
+        style={{
+          fontFamily: "var(--font-tsu-sahitya)",
+          width: "363px",
+          height: "91px",
+        }}
+        className="text-[96px] leading-[80px] font-normal text-black"
+      >
+        About Us
+      </p>
+    </div>
+  );
+};
+
+export default About;
