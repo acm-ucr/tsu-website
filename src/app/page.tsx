04f263bdeb28e41705ac59570b4d5fcce5d93d72
anitathakim <anitathakim@gmail.com>
2025-07-02 13:23:55 -0700

    added Why Join Us title with paragraph and left border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+    <div>
+      {/* Hello World */}
+
+      <div className="mx-auto mt-10 max-w-4xl font-serif">
+        <div className="mb-4 flex items-baseline gap-3">
+          <div className="h-7 border-l-5 border-orange-300"></div>
+          <h1 className="text-3xl">Why join us?</h1>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </div>
   );
 };
