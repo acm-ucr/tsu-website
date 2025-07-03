44c724a586c1192f7cbe8a2ab651cec0ae5ca3d0
cashums <136028501+cashums@users.noreply.github.com>
2025-07-03 09:10:09 +0900

    Reverted margins to flex standards rather than Figma definition

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,7 @@
 const About = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <p
-        style={{
-          fontFamily: "var(--font-tsu-sahitya)",
-          width: "363px",
-          height: "91px",
-        }}
-        className="text-[96px] leading-[80px] font-normal text-black"
-      >
+      <p className="font-tsu-sahitya h-[91px] w-[363px] text-[96px] leading-[80px] font-normal tracking-normal text-black">
         About Us
       </p>
     </div>
