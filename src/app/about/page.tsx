781d58da7a7a582aec5f5b22a6eb81d95bd2d7d7
cashums <136028501+cashums@users.noreply.github.com>
2025-07-03 10:30:49 +0900

    Resolved comments from review; simplified style by using defaults and Tailwind classes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <p className="font-tsu-sahitya h-[91px] w-[363px] text-[96px] leading-[80px] font-normal tracking-normal text-black">
+      <p className="font-tsu-sahitya text-8xl">
         About Us
       </p>
     </div>
