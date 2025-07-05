3b73e4057c567ae907fd3f9fd96ce43345b8ed11
stanleyx027 <sxiao027@ucr.edu>
2025-07-05 00:09:36 -0700

    Made changes

--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -8,16 +8,16 @@ interface BoardProps {
 
 const Card = ({ name, role }: BoardProps) => {
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center">
-      <div className="flex h-3/4 w-1/4 flex-shrink-0 border-10 border-white">
+    <div className="mb-12 flex flex-col items-center justify-center p-10">
+      <div className="relative flex w-80 flex-shrink-0 border-10 border-white">
         <Image src={Board} alt="Board" />
+        <Image
+          src={Flower}
+          alt="Flower"
+          className="absolute right-[15vw] -bottom-[8vh]"
+        />
       </div>
-      <Image
-        src={Flower}
-        alt="Flower"
-        className="absolute bottom-[15vh] left-[33vw]"
-      />
-      <div className="font-tsu-sahitya mt-2 flex flex-col items-center text-6xl">
+      <div className="font-tsu-sahitya mt-14 flex flex-col items-center text-5xl">
         <div className="text-tsu-brown-300 font-bold">{name}</div>
         <div className="ml-35">{role}</div>
       </div>
