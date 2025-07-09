5b78096b187b3472e926a5df03ec5d59feeed972
anitathakim <anitathakim@gmail.com>
2025-07-09 12:06:56 -0700

    ran prettier

--- src/components/home/whyJoinUs.tsx
+++ src/components/home/whyJoinUs.tsx
@@ -1,23 +1,26 @@
-"use client"
+"use client";
 
 const WhyJoinUs = () => {
-    return (
-        <div className="font-tsu-sahitya m-20 mx-auto max-w-4xl">
-            <div className="mb-4 flex items-baseline gap-3">
-                <div className="h-7 border-l-8" style={{ borderColor: "var(--color-tsu-orange-100)" }}/>
-                <h1 className="text-5xl">Why join us?</h1>
-            </div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-        </div>
-    );
-}
+  return (
+    <div className="font-tsu-sahitya m-20 mx-auto max-w-4xl">
+      <div className="mb-4 flex items-baseline gap-3">
+        <div
+          className="h-7 border-l-8"
+          style={{ borderColor: "var(--color-tsu-orange-100)" }}
+        />
+        <h1 className="text-5xl">Why join us?</h1>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
+};
 
 export default WhyJoinUs;
