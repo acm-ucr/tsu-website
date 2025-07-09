e3e35a4a9411e2beb6db01c07f605c58ddb442c5
Jamar Sutherland <jsutherland2024@gmail.com>
2025-07-08 20:12:44 -0700

    Fixed board header with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <p>
-        {" "}
-        <img src="/board header.png" alt="Board Header" />{" "}
-      </p>
+      <p className="font-tsu-sahitya text-8xl font-black">Meet The Board</p>
     </div>
   );
 };
