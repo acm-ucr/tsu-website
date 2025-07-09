7d618a5b02a18788f84ff6c6a27eb689280c8128
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2025-07-09 15:11:51 -0700

    Fixed Thai sections

--- components/ThaiStudentUnion.tsx
+++ components/ThaiStudentUnion.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 const ThaiStudentUnion = () => {
-    return (
-      <div className="bg-rose-50 p-8 flex border-l-8 border-pink-400">
-        <div>
-          <h1 className="text-6xl font-serif mb-4">Thai Student Union</h1>
-          <p className="text-xl font-serif">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-          </p>
-        </div>
+  return (
+    <div className="flex border-l-8 border-pink-400 bg-rose-50 p-8">
+      <div>
+        <h1 className="mb-4 font-serif text-6xl">Thai Student Union</h1>
+        <p className="font-serif text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi.
+        </p>
       </div>
-    );
-  };
-  
-  export default ThaiStudentUnion;
-  
+    </div>
+  );
+};
+
+export default ThaiStudentUnion;
