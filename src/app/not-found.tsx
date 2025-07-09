f20ac4509e0f5f8dbee7bb692f52d2a3bdbcabcf
Briana Garcia <briana_garcia_@outlook.com>
2025-07-08 22:59:55 -0700

    Added small fixes to 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="bg-tsu-beige-300 relative -z-10 flex h-screen w-full flex-col items-center justify-center gap-2 font-bold md:gap-4">
+    <div className="bg-tsu-beige-300 relative -z-10 flex h-[90vh] flex-col items-center justify-center gap-2 overflow-hidden font-bold md:gap-4">
       <div className="bg-tsu-orange-100 absolute right-0 -bottom-1/8 z-0 size-2/5 rounded-full opacity-50 blur-3xl md:-top-1/8" />
       <div className="bg-tsu-yellow-100 absolute -top-1/8 left-0 z-0 size-2/5 rounded-full opacity-50 blur-3xl md:top-auto md:-bottom-1/8" />
       <p className="font-tsu-sora z-10 text-3xl md:text-8xl">4 0 4</p>
