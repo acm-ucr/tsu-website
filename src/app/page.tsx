2b95f2f1001ba49a0a8634fe249c59f18f7ec054
anitathakim <anitathakim@gmail.com>
2025-07-09 11:29:20 -0700

    use colors and fronts from global.css and created WhyJoinUs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
 import WhoAreWe from "@/components/home/whoAreWe";
+import WhyJoinUs from "@/components/home/whyJoinUs";
 
 const Home = () => {
   return (
     <div>
       <WhoAreWe />
-
-      <div className="mx-auto mt-10 max-w-4xl font-serif">
-        <div className="mb-4 flex items-baseline gap-3">
-          <div className="h-7 border-l-5 border-orange-300"/>
-          <h1 className="text-3xl">Why join us?</h1>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
+      <WhyJoinUs />
     </div>
   );
 };
---
+++ src/components/home/whyJoinUs.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+const WhyJoinUs = () => {
+    return (
+        <div className="font-tsu-sahitya mx-auto mt-10 max-w-4xl">
+            <div className="mb-4 flex items-baseline gap-3">
+                <div className="h-7 border-l-5 border-color-tsu-orange-100"/>
+                <h1 className="text-3xl">Why join us?</h1>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    );
+}
+
+export default WhyJoinUs;
