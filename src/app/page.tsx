5aff66b34f397131ba940453874ebae1ee7f3ac0
Jamar Sutherland <jsutherland2024@gmail.com>
2025-07-09 12:16:39 -0700

    added back whoarewe portion I deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import WhoAreWe from "@/components/home/whoAreWe";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <p className="font-tsu-sahitya text-8xl font-black">Meet The Board</p>
-    </div>
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <p className="font-tsu-sahitya text-8xl font-black">Meet The Board</p>
+      </div>
+
+      <WhoAreWe />
+    </>
   );
 };
-
 export default Home;
