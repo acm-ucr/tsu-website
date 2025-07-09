94595762629fb0f039e7987d3cf17a11af01711f
Jamar Sutherland <jsutherland2024@gmail.com>
2025-07-09 12:30:18 -0700

    made board header into component and called it in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import WhoAreWe from "@/components/home/whoAreWe";
+import BoardHeader from "@/components/board/header";
 
 const Home = () => {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <p className="font-tsu-sahitya text-8xl font-black">Meet The Board</p>
-      </div>
-
       <WhoAreWe />
+      <BoardHeader />
     </>
   );
 };
---
+++ src/components/board/header.tsx
@@ -0,0 +1,10 @@
+const BoardHeader = () => {
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <p className="font-tsu-sahitya text-8xl font-black">Meet The Board</p>
+      </div>
+    </>
+  );
+};
+export default BoardHeader;
