5919d792dba5a2273b8684b1c10e41237ec657bf
cashums <136028501+cashums@users.noreply.github.com>
2025-07-10 12:30:04 +0900

    Removed <div> tag and included margins directly in <p> tag

--- src/components/about/header.tsx
+++ src/components/about/header.tsx
@@ -1,9 +1,5 @@
 const AboutHeader = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <p className="font-tsu-sahitya text-8xl">About Us</p>
-    </div>
-  );
+  return <p className="font-tsu-sahitya mt-63.5 ml-143.5 text-8xl">About Us</p>;
 };
 
 export default AboutHeader;
