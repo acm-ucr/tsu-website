8130c5aedafb166b3c2f507cb6a88bd00468f1ed
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2025-07-15 16:36:18 -0700

    Add JoinUs component and update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
-// "use client";
-
-// import ThaiStudentUnion from "../components/home/ThaiStudentUnion";
-// import WhoAreWe from "../components/home/whoAreWe";
-// import JoinUs from "../components/home/whyJoinUs";
-
-// export default function Home() {
-//   return (
-//     <main className="space-y-12">
-//       <ThaiStudentUnion />
-//       <WhoAreWe />
-//       <JoinUs />
-//     </main>
-//   );
-// }
-
 "use client";
 
 import ThaiStudentUnion from "../components/home/ThaiStudentUnion";
-// import WhoAreWe from "../components/home/whoAreWe"; // remove for now
-// import JoinUs from "../components/home/whyJoinUs"; // remove for now
 
 export default function Home() {
   return (
--- src/components/home/ThaiStudentUnion.tsx
+++ src/components/home/ThaiStudentUnion.tsx
@@ -1,197 +1,3 @@
-// // "use client";
-
-// // import Image from "next/image";
-// // import React from "react";
-
-// // const ThaiStudentUnion = () => {
-// //   return (
-// //     <main className="space-y-32 bg-rose-50 pb-20">
-// //       {/* Section 1: Thai Student Union Intro */}
-// //       <section className="relative flex flex-col md:flex-row border-l-8 border-pink-400 rounded-xl p-12 mx-4 bg-rose-50 overflow-hidden min-h-[400px]">
-// //         <div className="z-10 max-w-3xl">
-// //           <h1 className="mb-4 font-serif text-7xl">Thai Student Union</h1>
-// //           <p className="font-serif text-xl text-gray-700">
-// //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// //             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-// //             ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-// //           </p>
-// //         </div>
-
-// //         {/* Palm Tree Image */}
-// //         <Image
-// //           src="/palmTree.webp"
-// //           alt="Palm Tree"
-// //           width={320}
-// //           height={320}
-// //           className="absolute right-8 bottom-0 md:block hidden"
-// //         />
-// //       </section>
-
-// //       {/* Section 2: Why Join Us */}
-// //       <section className="relative border-l-8 border-yellow-400 rounded-xl bg-rose-50 px-12 pt-12 pb-20 mx-4 overflow-hidden min-h-[50px]">
-// //         <h2 className="text-5xl font-bold mb-4">Why Join Us?</h2>
-// //         <p className="font-serif text-lg text-gray-700 max-w-3xl">
-// //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-// //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-// //           minim veniam, quis nostrud exercitation ullamco laboris nisi. Lorem
-// //           ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-// //           tempor incididunt ut labore et dolore magna aliqua.
-// //         </p>
-
-// //         {/* Mango Images */}
-// //         <Image
-// //           src="/mango.webp"
-// //           alt="Mango Left"
-// //           width={120}
-// //           height={120}
-// //           className="absolute bottom-6 left-6 rotate-[20deg] md:block hidden"
-// //         />
-// //         <Image
-// //           src="/mango.webp"
-// //           alt="Mango Right"
-// //           width={120}
-// //           height={120}
-// //           className="absolute top-6 right-6 -rotate-[15deg] md:block hidden"
-// //         />
-// //         );
-// //   <div className="absolute bottom-0 left-0 w-9/10 h-3 bg-blue-400" />
-// //       </section>
-// //     </main>
-// //   );
-// // };
-
-// // export default ThaiStudentUnion;
-
-// "use client";
-
-// import Image from "next/image";
-// import React from "react";
-
-// const ThaiStudentUnion = () => {
-//   return (
-//     <main className="space-y-32 bg-rose-50 pb-20">
-//       {/* Section 1: Thai Student Union Intro */}
-//       <section className="relative flex flex-col md:flex-row border-l-8 border-pink-400 rounded-xl p-12 mx-4 bg-rose-50 overflow-hidden min-h-[400px]">
-//         <div className="z-10 max-w-3xl">
-//           <h1 className="mb-4 font-serif text-7xl">Thai Student Union</h1>
-//           <p className="font-serif text-xl text-gray-700">
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-//             ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-//           </p>
-//         </div>
-
-//         {/* Palm Tree Image */}
-//         <Image
-//           src="/palmTree.webp"
-//           alt="Palm Tree"
-//           width={320}
-//           height={320}
-//           className="absolute right-8 bottom-0 md:block hidden"
-//         />
-//       </section>
-
-//       {/* Section 2: Why Join Us */}
-//       <section className="relative border-l-8 border-yellow-400 rounded-xl bg-rose-50 px-12 pt-12 pb-20 mx-4 overflow-hidden min-h-[50px]">
-//         <h2 className="text-5xl font-bold mb-4">Why Join Us?</h2>
-//         <p className="font-serif text-lg text-gray-700 max-w-3xl">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim veniam, quis nostrud exercitation ullamco laboris nisi. Lorem
-//           ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-//           tempor incididunt ut labore et dolore magna aliqua.
-//         </p>
-
-//         {/* Mango Images */}
-//         <Image
-//           src="/mango.webp"
-//           alt="Mango Left"
-//           width={120}
-//           height={120}
-//           className="absolute bottom-6 left-6 rotate-[20deg] md:block hidden"
-//         />
-//         <Image
-//           src="/mango.webp"
-//           alt="Mango Right"
-//           width={120}
-//           height={120}
-//           className="absolute top-6 right-6 -rotate-[15deg] md:block hidden"
-//         />
-//       </section>
-
-//       {/* Blue bar separate from the yellow section */}
-//       <div className="mx-4 mt-4 h-3 w-[90%] bg-blue-400 rounded" />
-//     </main>
-//   );
-// };
-
-// export default ThaiStudentUnion;
-
-// "use client";
-
-// import Image from "next/image";
-// import React from "react";
-
-// const ThaiStudentUnion = () => {
-//   return (
-//     <main className="space-y-32 bg-rose-50 pb-20">
-//       {/* Section 1: Thai Student Union Intro */}
-//       <section className="relative flex flex-col md:flex-row border-l-8 border-pink-400 rounded-xl p-12 mx-4 bg-rose-50 overflow-hidden min-h-[400px]">
-//         <div className="z-10 max-w-3xl">
-//           <h1 className="mb-4 font-serif text-7xl">Thai Student Union</h1>
-//           <p className="font-serif text-xl text-gray-700">
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-//             ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-//           </p>
-//         </div>
-
-//         {/* Palm Tree Image */}
-//         <Image
-//           src="/palmTree.webp"
-//           alt="Palm Tree"
-//           width={320}
-//           height={320}
-//           className="absolute right-8 bottom-0 md:block hidden"
-//         />
-//       </section>
-
-//       {/* Section 2: Why Join Us */}
-//       <section className="relative border-l-8 border-yellow-400 rounded-xl bg-rose-50 px-12 pt-12 pb-20 mx-4 overflow-hidden min-h-[50px]">
-//         <h2 className="text-5xl font-bold mb-4">Why Join Us?</h2>
-//         <p className="font-serif text-lg text-gray-700 max-w-3xl">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim veniam, quis nostrud exercitation ullamco laboris nisi. Lorem
-//           ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-//           tempor incididunt ut labore et dolore magna aliqua.
-//         </p>
-
-//         {/* Mango Images */}
-//         <Image
-//           src="/mango.webp"
-//           alt="Mango Left"
-//           width={120}
-//           height={120}
-//           className="absolute bottom-6 left-6 rotate-[20deg] md:block hidden"
-//         />
-//         <Image
-//           src="/mango.webp"
-//           alt="Mango Right"
-//           width={120}
-//           height={120}
-//           className="absolute top-6 right-6 -rotate-[15deg] md:block hidden"
-//         />
-
-//         {/* Blue Bottom Border */}
-//         <div className="absolute bottom-0 left-0 w-full h-3 bg-blue-400" />
-//       </section>
-//     </main>
-//   );
-// };
-
-// export default ThaiStudentUnion;
-
 "use client";
 
 import Image from "next/image";
@@ -217,7 +23,7 @@ const ThaiStudentUnion = () => {
           alt="Palm Tree"
           width={500}
           height={500}
-          className="absolute bottom-[-15] left-[600] hidden md:block"
+          className="absolute bottom-[-15px] left-[600px] hidden md:block"
         />
       </section>
 
@@ -244,14 +50,14 @@ const ThaiStudentUnion = () => {
           alt="Mango Left"
           width={200}
           height={200}
-          className="absolute bottom-[-65] left-[-35] hidden scale-x-[1] -rotate-[-180deg] md:block"
+          className="absolute bottom-[-65px] left-[-35px] hidden scale-x-[1] -rotate-[180deg] md:block"
         />
         <Image
           src="/mango.webp"
           alt="Mango Right"
           width={200}
           height={200}
-          className="absolute top-[-25] left-[715] hidden rotate-[15deg] md:block"
+          className="absolute top-[-25px] left-[715px] hidden rotate-[15deg] md:block"
         />
       </section>
 
