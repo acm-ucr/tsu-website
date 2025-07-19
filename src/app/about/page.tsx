d1edb455f43aac53791abb88a5d41692ef0acd90
Yubin Zhen <yubinzhen020105@gmail.com>
2025-07-18 21:44:21 -0700

    added polaroids

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutHeader from "../../components/about/header";
+import Polaroid from "../../components/about/polaroid";
 
 const About = () => {
   return (
     <>
       <AboutHeader />
+      <Polaroid />
     </>
   );
 };
---
+++ src/components/about/polaroid.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Image from "next/image";
+import polaroid from "@/public/polaroid.webp";
+import polaroid2 from "@/public/polaroid2.webp";
+
+const Polaroid = () => {
+  return (
+    <div className="bg-tsu-beige-300 flex h-auto w-screen">
+      <Image
+        src={polaroid}
+        alt="Polaroid 1 Image"
+        className="mt-10 ml-35 p-13"
+      />
+
+      <Image src={polaroid2} alt="Polaroid 2 Image" className="ml-17 p-13" />
+    </div>
+  );
+};
+
+export default Polaroid;
