278c97dbb4c83fafa305116282a13364fe6cc0ce
Briana Garcia <briana_garcia_@outlook.com>
2025-07-19 17:44:59 -0700

    Added top About Us text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import AboutHeader from "../../components/about/header";
 import Polaroid from "../../components/about/polaroid";
+import { TopAboutText } from "@/components/about/toptext";
 
 const About = () => {
   return (
     <div className="bg-tsu-beige-300">
       <AboutHeader />
+      <TopAboutText />
       <Polaroid />
     </div>
   );
---
+++ src/components/about/toptext.tsx
@@ -0,0 +1,15 @@
+export const TopAboutText = () => {
+  return (
+    <div>
+      <div className="bg-tsu-green-100 h-3 w-3/5" />
+      <p className="font-tsu-sahitya m-8 ml-20 w-4/5 text-3xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi
+      </p>
+    </div>
+  );
+};
