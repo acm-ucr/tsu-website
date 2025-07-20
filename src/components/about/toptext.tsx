ef497067633d4cb2906c82e8906643710c991d5c
Briana Garcia <briana_garcia_@outlook.com>
2025-07-19 17:50:56 -0700

    About Us Text responsiveness

--- src/components/about/toptext.tsx
+++ src/components/about/toptext.tsx
@@ -1,8 +1,8 @@
 export const TopAboutText = () => {
   return (
     <div>
-      <div className="bg-tsu-green-100 h-3 w-3/5" />
-      <p className="font-tsu-sahitya m-8 ml-20 w-4/5 text-3xl">
+      <div className="bg-tsu-green-100 h-1 w-3/5 md:h-3" />
+      <p className="font-tsu-sahitya m-6 w-4/5 text-xs md:m-8 md:ml-20 md:text-3xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi Lorem ipsum dolor
