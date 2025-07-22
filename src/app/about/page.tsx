2864ab9e9934216ab89f515d68bda5481af590a6
cashums <136028501+cashums@users.noreply.github.com>
2025-07-22 12:13:47 +0900

    Bottom text and green bar divider implemented

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import AboutHeader from "../../components/about/header";
 import Polaroid from "../../components/about/polaroid";
+import BottomText from "../../components/about/bottomtext";
 import Elephant from "../../components/about/elephant";
 
 const About = () => {
@@ -7,6 +8,7 @@ const About = () => {
     <div className="bg-tsu-beige-300">
       <AboutHeader />
       <Polaroid />
+      <BottomText />
       <Elephant />
     </div>
   );
---
+++ src/components/about/bottomtext.tsx
@@ -0,0 +1,10 @@
+const BottomText = () => {
+  return (
+    <>
+      <div className="bg-green-500 h-2 w-1/2 ml-auto mt-8"></div>
+      <p className="font-tsu-sahitya py-20 ml-auto mr-10 w-3/4 text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sequi alias dolor facere atque quo quis dolorum dicta tenetur vero aut ratione consectetur veniam doloremque iusto odio, beatae quae earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius alias fugiat autem, deleniti ratione minima sint reiciendis perspiciatis repellat nihil porro illo nesciunt cum error quibusdam eaque harum quidem reprehenderit!</p>
+    </>
+  );
+};
+
+export default BottomText;
