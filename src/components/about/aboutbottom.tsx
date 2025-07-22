4b684f3c7d8819d21100889b15d0eee9d753da62
Hannah Hwang <hannahhwang@mac.mynetworksettings.com>
2025-07-22 15:35:41 -0700

    format

--- src/components/about/aboutbottom.tsx
+++ src/components/about/aboutbottom.tsx
@@ -5,7 +5,7 @@ const AboutBottom = () => {
   return (
     <>
       <div className="mt-20 ml-auto h-2 w-1/2 bg-green-500"></div>
-      <p className="font-tsu-sahitya mr-10 mb-15 ml-auto w-3/5 py-15 text-2xl text-right">
+      <p className="font-tsu-sahitya mr-10 mb-15 ml-auto w-3/5 py-15 text-right text-2xl">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sequi
         alias dolor facere atque quo quis dolorum dicta tenetur vero aut ratione
         consectetur veniam doloremque iusto odio, beatae quae earum. Lorem ipsum
