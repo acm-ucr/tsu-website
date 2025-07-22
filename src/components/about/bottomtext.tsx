6878a8547ff0061d5aba9113c9c948e15113b716
cashums <136028501+cashums@users.noreply.github.com>
2025-07-22 12:35:21 +0900

    Ran Prettier

--- src/components/about/bottomtext.tsx
+++ src/components/about/bottomtext.tsx
@@ -1,8 +1,16 @@
 const BottomText = () => {
   return (
     <>
-      <div className="bg-green-500 h-2 w-1/2 ml-auto mt-8"></div>
-      <p className="font-tsu-sahitya py-15 ml-auto mr-10 w-3/4 text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sequi alias dolor facere atque quo quis dolorum dicta tenetur vero aut ratione consectetur veniam doloremque iusto odio, beatae quae earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius alias fugiat autem, deleniti ratione minima sint reiciendis perspiciatis repellat nihil porro illo nesciunt cum error quibusdam eaque harum quidem reprehenderit!</p>
+      <div className="mt-8 ml-auto h-2 w-1/2 bg-green-500"></div>
+      <p className="font-tsu-sahitya mr-10 ml-auto w-3/4 py-15 text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sequi
+        alias dolor facere atque quo quis dolorum dicta tenetur vero aut ratione
+        consectetur veniam doloremque iusto odio, beatae quae earum. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Eius alias fugiat autem,
+        deleniti ratione minima sint reiciendis perspiciatis repellat nihil
+        porro illo nesciunt cum error quibusdam eaque harum quidem
+        reprehenderit!
+      </p>
     </>
   );
 };
--- src/components/about/elephant.tsx
+++ src/components/about/elephant.tsx
@@ -7,7 +7,7 @@ const Elephant = () => {
       <Image
         src={elephant}
         alt="Elephant w/Text Bubble Image"
-        className="flex w-[80%] ml-40 -mb-30"
+        className="-mb-30 ml-40 flex w-[80%]"
       />
     </div>
   );
