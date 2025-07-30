0600c6f81b5817e5cf9f8eaa926bb5c02fc40901
Briana Garcia <briana_garcia_@outlook.com>
2025-07-30 00:47:14 -0700

    Adjust About Page + Responsiveness

--- src/components/about/polaroid.tsx
+++ src/components/about/polaroid.tsx
@@ -4,14 +4,9 @@ import polaroid2 from "@/public/polaroid2.webp";
 
 const Polaroid = () => {
   return (
-    <div className="flex">
-      <Image
-        src={polaroid}
-        alt="Polaroid 1 Image"
-        className="mt-10 ml-65 p-13"
-      />
-
-      <Image src={polaroid2} alt="Polaroid 2 Image" className="ml-17 p-13" />
+    <div className="flex items-center justify-center gap-5 px-4 py-2 md:p-8">
+      <Image src={polaroid} alt="Polaroid 1 Image" className="mt-6 w-2/5" />
+      <Image src={polaroid2} alt="Polaroid 2 Image" className="-mt-6 w-2/5" />
     </div>
   );
 };
--- src/components/about/toptext.tsx
+++ src/components/about/toptext.tsx
@@ -1,7 +1,7 @@
 export const TopAboutText = () => {
   return (
     <div>
-      <div className="bg-tsu-green-100 h-1 w-3/5 md:h-3" />
+      <div className="bg-tsu-green-100 h-1 w-1/2 md:h-3" />
       <p className="font-tsu-sahitya m-6 w-4/5 text-xs md:m-8 md:ml-20 md:text-3xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
