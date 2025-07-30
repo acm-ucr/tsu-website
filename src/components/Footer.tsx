c2b99eaa4c985ee01b9b83f6eca676f8afd340f7
stanleyx027 <sxiao027@ucr.edu>
2025-07-29 22:42:05 -0700

    Made changes based on the comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <Image
         src={TSULogo}
         alt="TSU Logo"
-        className="flex w-25 flex-shrink-0 sm:w-[12%]"
+        className="mt-5 flex w-25 flex-shrink-0 sm:mt-0 sm:w-[12%]"
       />
 
       <div className="font-tsu-sahitya flex items-center text-center text-3xl whitespace-nowrap sm:items-start sm:text-left md:text-4xl lg:text-5xl 2xl:text-6xl">
@@ -19,17 +19,17 @@ const Footer = () => {
 
       <div className="invisible border-1 border-black sm:visible sm:h-20 md:ml-2 lg:ml-10"></div>
 
-      <div className="font-tsu-sahitya mb-2 flex items-center gap-5 text-xl font-medium xl:ml-10 xl:gap-10 xl:text-3xl 2xl:text-4xl">
+      <div className="font-tsu-sahitya mb-3 flex items-center gap-5 text-xl font-medium sm:mb-0 xl:ml-10 xl:gap-10 xl:text-3xl 2xl:text-4xl">
         <div className="flex items-center gap-2">
           <Link href="https://www.instagram.com/tsuucr/?hl=en" target="_blank">
-            <AiFillInstagram className="text-tsu-orange-100 h-11 w-11 xl:h-15 xl:w-15" />
+            <AiFillInstagram className="text-tsu-orange-100 h-8 w-8 sm:h-11 sm:w-11 xl:h-15 xl:w-15" />
           </Link>
           <span>Instagram</span>
         </div>
 
         <div className="flex items-center gap-2">
           <Link href="https://line.me/ti/g/3K_UGDcRGR" target="_blank">
-            <FaLine className="text-tsu-orange-100 h-11 w-11 xl:h-15 xl:w-15" />
+            <FaLine className="text-tsu-orange-100 h-8 w-8 sm:h-11 sm:w-11 xl:h-15 xl:w-15" />
           </Link>
           <span>Line</span>
         </div>
