3eaf4ccc7bcceaac50373dc654582393520ea920
cashums <136028501+cashums@users.noreply.github.com>
2025-07-30 08:52:43 -0700

    Implemented mobile responsiveness for Board page (1 column instead of 3)

--- src/components/board/CardMapping.tsx
+++ src/components/board/CardMapping.tsx
@@ -4,7 +4,7 @@ import Card from "@/components/board/Card";
 const CardMapping = () => {
   return (
     <div className="flex flex-col items-center justify-center p-10">
-      <div className="grid grid-cols-3 gap-x-20">
+      <div className="grid grid-cols-1 gap-x-20 md:grid-cols-3">
         {boardMembers.map(({ name, role, image }, index) => (
           <Card key={index} name={name} role={role} image={image} />
         ))}
