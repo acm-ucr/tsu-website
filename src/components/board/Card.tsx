cef509eae2e7355cf133146013a3737c2731ec40
Hannah Hwang <hannahhwang@mac.mynetworksettings.com>
2025-07-31 22:14:16 -0700

    fixes

--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -10,17 +10,17 @@ interface BoardProps {
 const Card = ({ name, role, image }: BoardProps) => {
   return (
     <div className="mb-5 flex flex-col items-center justify-center p-4 md:p-10">
-      <div className="relative flex w-60 flex-shrink-0 border-8 border-white md:w-80 md:border-10">
+      <div className="relative flex w-60 flex-shrink-0 border-8 border-white md:border-10 lg:w-70 xl:w-90">
         <Image src={image || Board} alt="Board" />
         <Image
           src={Flower}
           alt="Flower"
-          className="absolute right-[150] bottom-[-40] w-30 md:right-[200] md:bottom-[-50] md:w-auto"
+          className="absolute right-[170] bottom-[-30] w-20 lg:right-[195] lg:bottom-[-20] lg:w-25 xl:right-[235] xl:bottom-[-35] xl:w-35"
         />
       </div>
-      <div className="font-tsu-sahitya mt-8 flex flex-col items-center text-3xl md:mt-14 md:text-5xl">
-        <div className="text-tsu-brown-300 font-bold">{name}</div>
-        <div className="ml-20 md:ml-35">{role}</div>
+      <div className="font-tsu-sahitya mt-8 flex flex-col items-center text-3xl md:text-4xl xl:text-5xl">
+        <div className="text-tsu-brown-300 ml-[-70] font-bold">{name}</div>
+        <div className="ml-15">{role}</div>
       </div>
     </div>
   );
