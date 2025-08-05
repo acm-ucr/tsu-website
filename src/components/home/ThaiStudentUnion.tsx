5797251f4ede1607f3f01ca2a1dc647cc5d5d9b5
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2025-08-05 16:44:36 -0700

    Format fixed

--- src/components/home/ThaiStudentUnion.tsx
+++ src/components/home/ThaiStudentUnion.tsx
@@ -1,34 +1,3 @@
-// import Image from "next/image";
-// import React from "react";
-
-// const ThaiStudentUnion = () => {
-//   return (
-//     <div className="relative space-y-32 bg-[var(--color-bg-rose-50)] pb-20">
-//       <div className="relative mx-4 flex min-h-[25vh] flex-col overflow-hidden rounded-xl border-l-8 border-[var(--color-border-pink-400)] bg-[var(--color-bg-rose-50)] p-12 md:flex-row">
-//         <div className="z-10 max-w-3xl">
-//           <p className="mb-4 font-serif text-7xl">Thai Student Union</p>
-//           <p className="font-serif text-xl text-[var(--color-text-gray-700)]">
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-//             ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-//           </p>
-//         </div>
-
-//         {/* Palm Tree Image */}
-//         <Image
-//           src="/palmTree.webp"
-//           alt="Palm Tree"
-//           width={200}
-//           height={200}
-//           className="absolute bottom-[1] left-[35vw] hidden md:block w-[15vw] h-auto"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ThaiStudentUnion;
-
 import Image from "next/image";
 import PalmTree from "@/public/palmTree.webp";
 
