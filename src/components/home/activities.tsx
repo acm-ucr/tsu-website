63d67bfac77fc7a94f459664668df433f667e15e
anitathakim <anitathakim@gmail.com>
2025-08-07 07:52:04 +0700

    ran npm format

--- src/components/home/activities.tsx
+++ src/components/home/activities.tsx
@@ -2,32 +2,32 @@
 import Image, { StaticImageData } from "next/image";
 
 type ActivitiesProps = {
-  backgroundColor: string; 
-  borderColor: string;  
+  backgroundColor: string;
+  borderColor: string;
   image: StaticImageData;
   activityName: string;
   activityDesc: string;
 };
-const Activities = ({ backgroundColor, borderColor, image, activityName, activityDesc }: ActivitiesProps) => {
-    return (
-       <div className= {`flex h-auto w-screen ${backgroundColor}`}>
-            <Image
-              src={image}
-              alt={activityName}
-              className="ml-17 w-1/3 p-13"
-            />
-      
-            <div className="font-tsu-sahitya mt-10 mb-4">
-                <div className="mb-4 flex items-baseline gap-3">
-                    <div className={`h-10 border-l-8 ${borderColor}`} />
-                    <p className="text-5xl text-white ">{activityName}</p>
-                </div>
-              <p className="text-2xl text-white mr-30">
-                {activityDesc}
-              </p>
-            </div>
+const Activities = ({
+  backgroundColor,
+  borderColor,
+  image,
+  activityName,
+  activityDesc,
+}: ActivitiesProps) => {
+  return (
+    <div className={`flex h-auto w-screen ${backgroundColor}`}>
+      <Image src={image} alt={activityName} className="ml-17 w-1/3 p-13" />
+
+      <div className="font-tsu-sahitya mt-10 mb-4">
+        <div className="mb-4 flex items-baseline gap-3">
+          <div className={`h-10 border-l-8 ${borderColor}`} />
+          <p className="text-5xl text-white">{activityName}</p>
         </div>
-    );
-  };
-  
-  export default Activities;
+        <p className="mr-30 text-2xl text-white">{activityDesc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Activities;
