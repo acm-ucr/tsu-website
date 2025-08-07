3dd01443819d807c94e1a842e52ae267c41a9be0
anitathakim <anitathakim@gmail.com>
2025-08-07 08:14:04 +0700

    fixed margins and added Songkran component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
       <ThaiStudentUnion />
       <WhoAreWe />
       <WhyJoinUs />
-      {/* <Activities backgroundColor="bg-tsu-pink-100" borderColor="bg-tsu-pink-200" image={placeholder} activityName="Loy Krathong" activityDesc="Loy Krathong is the water lantern festival that happens on the full-moon day of the twelfth month of the lunar calendar. “Loy” means “to float” and “krathong” refers to the small, floating offerings. The purpose of Loy Krathong is to let go of bad energy and bad luck and let it flow down the river to let the water goddess, Phra Mae Khongkha, cleanse the energy and luck. It also serves as a thank you to Phra Mae Khongkha for providing resources through water, as well as an apology to her for how we use those resources." />     */}
       <LoyKrathong />
     </div>
   );
--- src/components/home/activities.tsx
+++ src/components/home/activities.tsx
@@ -17,14 +17,14 @@ const Activities = ({
 }: ActivitiesProps) => {
   return (
     <div className={`flex h-auto w-screen ${backgroundColor}`}>
-      <Image src={image} alt={activityName} className="ml-17 w-1/3 p-13" />
+      <Image src={image} alt={activityName} className="w-1/3 p-13" />
 
-      <div className="font-tsu-sahitya mt-10 mb-4">
+      <div className="font-tsu-sahitya mt-14">
         <div className="mb-4 flex items-baseline gap-3">
           <div className={`h-10 border-l-8 ${borderColor}`} />
           <p className="text-5xl text-white">{activityName}</p>
         </div>
-        <p className="mr-30 text-2xl text-white">{activityDesc}</p>
+        <p className="mr-30 mb-14 text-2xl text-white">{activityDesc}</p>
       </div>
     </div>
   );
