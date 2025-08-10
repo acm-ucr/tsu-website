3bdec5121844f790066411c43d6e3cd54c931b7c
cashums <136028501+cashums@users.noreply.github.com>
2025-08-10 16:05:04 -0700

    Added a simple slide-up animation to bottom text

--- src/components/about/aboutbottom.tsx
+++ src/components/about/aboutbottom.tsx
@@ -8,7 +8,12 @@ const AboutBottom = () => {
     <div className="flex-col justify-items-center">
       <div className="justify-items-end pt-4">
         <div className="bg-tsu-green-100 h-1 w-1/2 md:h-3"></div>
-        <p className="font-tsu-sahitya m-6 w-4/5 text-xs md:m-8 md:text-3xl">
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="font-tsu-sahitya m-6 w-4/5 text-xs md:m-8 md:text-3xl"
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sequi
           alias dolor facere atque quo quis dolorum dicta tenetur vero aut
           ratione consectetur veniam doloremque iusto odio, beatae quae earum.
@@ -16,7 +21,7 @@ const AboutBottom = () => {
           fugiat autem, deleniti ratione minima sint reiciendis perspiciatis
           repellat nihil porro illo nesciunt cum error quibusdam eaque harum
           quidem reprehenderit!
-        </p>
+        </motion.p>
       </div>
       <Image
         src={elephant}
