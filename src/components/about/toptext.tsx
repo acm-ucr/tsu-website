93f31228fb8e7f2c63155b556c32fee5e6fe5c69
cashums <136028501+cashums@users.noreply.github.com>
2025-08-10 16:16:01 -0700

    Added slide-up animation to top text

--- src/components/about/toptext.tsx
+++ src/components/about/toptext.tsx
@@ -1,15 +1,23 @@
+"use client";
+import { motion } from "motion/react";
+
 export const TopAboutText = () => {
   return (
     <div>
       <div className="bg-tsu-green-100 h-1 w-1/2 md:h-3" />
-      <p className="font-tsu-sahitya m-6 w-4/5 text-xs md:m-8 md:ml-20 md:text-3xl">
+      <motion.p
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="font-tsu-sahitya m-6 w-4/5 text-xs md:m-8 md:ml-20 md:text-3xl"
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi Lorem ipsum dolor
         sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco laboris nisi
-      </p>
+      </motion.p>
     </div>
   );
 };
