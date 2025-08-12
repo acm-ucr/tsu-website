730866af5d3b20a7c98ff18e656c86408b3e9164
Briana Garcia <briana_garcia_@outlook.com>
2025-08-12 16:25:28 -0700

    Animate Board Page

--- src/components/board/header.tsx
+++ src/components/board/header.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { motion } from "motion/react";
 const BoardHeader = () => {
   return (
-    <p className="font-tsu-sahitya pt-10 text-center text-5xl md:pt-20 md:text-8xl">
+    <motion.p
+      initial={{ opacity: 0, y: 10 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="font-tsu-sahitya pt-10 text-center text-5xl md:pt-20 md:text-8xl"
+    >
       Meet The Board
-    </p>
+    </motion.p>
   );
 };
 export default BoardHeader;
