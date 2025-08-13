12c289927c2612857bae219e004f6ec23d64bfde
Briana Garcia <briana_garcia_@outlook.com>
2025-08-13 14:55:13 -0700

    Board animation fixes

--- src/components/board/header.tsx
+++ src/components/board/header.tsx
@@ -1,15 +1,9 @@
 "use client";
-import { motion } from "motion/react";
 const BoardHeader = () => {
   return (
-    <motion.p
-      initial={{ opacity: 0, y: 10 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      className="font-tsu-sahitya pt-10 text-center text-5xl md:pt-20 md:text-8xl"
-    >
+    <p className="font-tsu-sahitya pt-10 text-center text-5xl md:pt-20 md:text-8xl">
       Meet The Board
-    </motion.p>
+    </p>
   );
 };
 export default BoardHeader;
