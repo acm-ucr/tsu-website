9bc580a8d16faaf0f3cc6f2970f2790814ac8f1a
stanleyx027 <sxiao027@ucr.edu>
2025-08-13 01:54:33 -0700

    Added edits and animations for Nav and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import TSULogo from "@/public/logo.webp";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaLine } from "react-icons/fa";
+import { motion } from "motion/react";
 
 const Footer = () => {
   return (
@@ -12,25 +14,38 @@ const Footer = () => {
         alt="TSU Logo"
         className="mt-5 flex w-25 sm:mt-0 sm:w-[12%]"
       />
-
-      <div className="font-tsu-sahitya flex text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl">
-        Thai Student Union
+      <div className="font-tsu-sahitya flex flex-col items-center">
+        <div className="text-3xl sm:text-2xl md:text-4xl lg:text-5xl 2xl:text-6xl">
+          Thai Student Union
+        </div>
+        <div className="text-center text-xs md:text-sm">
+          © ACM at UCR 2025.{" "}
+          <br className="sm:inline sm:text-center md:hidden" />
+          Made with 🧡 from ACM & TSU
+        </div>
       </div>
 
       <div className="invisible border-1 border-black sm:visible sm:h-20 md:ml-2 lg:ml-10"></div>
 
       <div className="font-tsu-sahitya mb-5 flex items-center gap-5 text-xl font-medium sm:mb-0 xl:ml-10 xl:gap-10 xl:text-3xl 2xl:text-4xl">
         <div className="flex items-center gap-2">
-          <Link href="https://www.instagram.com/tsuucr/?hl=en" target="_blank">
-            <AiFillInstagram className="text-tsu-orange-100 h-8 w-8 sm:h-11 sm:w-11 xl:h-15 xl:w-15" />
-          </Link>
+          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
+            <Link
+              href="https://www.instagram.com/tsuucr/?hl=en"
+              target="_blank"
+            >
+              <AiFillInstagram className="text-tsu-orange-100 h-8 w-8 sm:h-11 sm:w-11 xl:h-15 xl:w-15" />
+            </Link>
+          </motion.div>
           <span>Instagram</span>
         </div>
 
         <div className="flex items-center gap-2">
-          <Link href="https://line.me/ti/g/3K_UGDcRGR" target="_blank">
-            <FaLine className="text-tsu-orange-100 h-8 w-8 sm:h-11 sm:w-11 xl:h-15 xl:w-15" />
-          </Link>
+          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
+            <Link href="https://line.me/ti/g/3K_UGDcRGR" target="_blank">
+              <FaLine className="text-tsu-orange-100 h-8 w-8 sm:h-11 sm:w-11 xl:h-15 xl:w-15" />
+            </Link>
+          </motion.div>
           <span>Line</span>
         </div>
       </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,37 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { navigations } from "@/data/NavBarLinks";
+import navigations from "@/data/NavBarLinks";
 import logo from "@/public/textLogo.webp";
+import { usePathname } from "next/navigation";
+import { motion } from "motion/react";
 
 const NavBar = () => {
+  const path = usePathname();
   return (
     <div className="bg-tsu-beige-300 relative flex w-full items-center justify-between px-8 py-3 shadow-lg">
-      <Link href="/">
-        <Image src={logo} alt="TSU Text Logo" width={200} height={70} />
-      </Link>
+      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+        <Link href="/">
+          <Image src={logo} alt="TSU Text Logo" width={200} height={70} />
+        </Link>
+      </motion.div>
+
       <div className="font-tsu-sahitya flex w-2/3 justify-end gap-8 px-8 text-4xl">
         {navigations.map(({ name, link }, index) => (
-          <Link href={link} key={index} className="px-2">
-            {" "}
-            {name}{" "}
-          </Link>
+          <motion.div whileHover={{ scale: 1.1 }} key={index}>
+            <Link
+              href={link}
+              className={`${
+                path === link
+                  ? "text-tsu-orange-200"
+                  : "hover:text-tsu-orange-200"
+              }`}
+            >
+              {" "}
+              {name}{" "}
+            </Link>
+          </motion.div>
         ))}
       </div>
     </div>
