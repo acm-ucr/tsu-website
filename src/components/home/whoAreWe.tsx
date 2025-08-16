0404c606352c5d3e1fa768fddbce6e31f50cd0b9
Jamar Sutherland <jsutherland2024@gmail.com>
2025-08-16 14:50:19 -0700

    forgot to center the whoarewe header

--- src/components/home/whoAreWe.tsx
+++ src/components/home/whoAreWe.tsx
@@ -8,12 +8,12 @@ const WhoAreWe = () => {
       <Image
         src={placeholder}
         alt="Placeholder Image"
-        className="ml-0 w-full p-10 md:ml-9 md:w-1/3 md:p-5 lg:ml-17 lg:w-1/3 lg:p-13"
+        className="ml-0 w-full p-10 md:ml-9 md:w-1/3 md:p-5 lg:ml-17 lg:w-1/3 lg:p-7"
       />
 
-      <div className="font-tsu-sahitya mb-10 ml-11 flex w-4/5 flex-col text-white md:mt-4 md:mb-0 md:ml-9 md:w-2/3 lg:mt-8 lg:ml-25 lg:w-5/10">
-        <div className="mb-1 flex items-baseline gap-1.5 lg:mb-4 lg:gap-3">
-          <div className="border-tsu-pink-100 h-6 border-l-4 md:border-l-8 lg:h-10" />
+      <div className="font-tsu-sahitya mb-10 ml-10 flex w-4/5 flex-col text-white md:mt-4 md:mb-5 md:ml-9 md:w-2/3 lg:mt-8 lg:ml-25 lg:w-5/10">
+        <div className="mx-auto mb-1 flex items-baseline gap-1 lg:mb-4 lg:gap-3">
+          <div className="border-tsu-pink-100 mx-auto h-4 border-l-4 md:h-6 md:border-l-8 lg:h-10" />
           <p className="text-lg text-white md:text-3xl lg:text-5xl">
             Who Are We?
           </p>
