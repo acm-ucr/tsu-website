1c19386edcd0bae446ed8dbd9eb938d75536cc9c
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2025-08-19 10:40:10 -0700

    Fixed comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-// import ThaiStudentUnion from "@/components/home/ThaiStudentUnion";
-// import WhyJoinUs from "@/components/home/whyJoinUs";
-
-// export default function Home() {
-//   return (
-//     <div className="bg-rose-50">
-//       <ThaiStudentUnion />
-//       <WhyJoinUs />
-//     </div>
-//   );
-// }
-
 import ThaiStudentUnion from "@/components/home/ThaiStudentUnion";
 import WhoAreWe from "@/components/home/whoAreWe";
 import WhyJoinUs from "@/components/home/whyJoinUs";
--- src/components/home/ThaiStudentUnion.tsx
+++ src/components/home/ThaiStudentUnion.tsx
@@ -1,28 +1,3 @@
-// import Image from "next/image";
-// import PalmTree from "@/public/palmTree.webp";
-
-// const ThaiStudentUnion = () => {
-//   return (
-//     <div className="relative ml-30 min-h-[60vh] py-20">
-//       <div className="border-tsu-pink-100 border-l-8 px-10">
-//         <p className="mb-4 font-serif text-8xl">Thai Student Union</p>
-//         <p className="w-[60vw] font-serif text-2xl">
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim veniam, quis nostrud exercitation ullamco laboris nisi.
-//         </p>
-//       </div>
-//       <Image
-//         src={PalmTree}
-//         alt="Palm Tree"
-//         className="absolute right-[240] bottom-[75] w-[32vw]"
-//       />
-//     </div>
-//   );
-// };
-
-// export default ThaiStudentUnion;
-
 import Image from "next/image";
 import PalmTree from "@/public/palmTree.webp";
 
