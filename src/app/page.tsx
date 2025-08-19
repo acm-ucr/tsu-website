7c8ac573d627e7c1bdcf87455e3b369866377690
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2025-08-19 10:37:37 -0700

    Fixed images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
-import ThaiStudentUnion from "../components/home/ThaiStudentUnion";
-import WhoAreWe from "../components/home/whoAreWe";
-import WhyJoinUs from "../components/home/whyJoinUs";
+// import ThaiStudentUnion from "@/components/home/ThaiStudentUnion";
+// import WhyJoinUs from "@/components/home/whyJoinUs";
+
+// export default function Home() {
+//   return (
+//     <div className="bg-rose-50">
+//       <ThaiStudentUnion />
+//       <WhyJoinUs />
+//     </div>
+//   );
+// }
+
+import ThaiStudentUnion from "@/components/home/ThaiStudentUnion";
+import WhoAreWe from "@/components/home/whoAreWe";
+import WhyJoinUs from "@/components/home/whyJoinUs";
 import LoyKrathong from "@/components/home/LoyKrathong";
 
 export default function Home() {
   return (
-    <div className="bg-tsu-beige-300">
+    <>
       <ThaiStudentUnion />
       <WhoAreWe />
       <WhyJoinUs />
       <LoyKrathong />
-    </div>
+    </>
   );
 }
--- src/components/home/whyJoinUs.tsx
+++ src/components/home/whyJoinUs.tsx
@@ -1,25 +1,29 @@
-"use client";
 import Image from "next/image";
 import mango from "@/public/mango.webp";
 import mango2 from "@/public/mango2.webp";
-import mango3 from "@/public/mango3.webp";
 
 const WhyJoinUs = () => {
   return (
-    <div className="font-tsu-sahitya relative mx-auto mt-15 w-[85vw] py-20 2xl:my-30 2xl:mb-40">
-      <div className="absolute top-0 right-[-5] mt-5 hidden translate-x-1/2 2xl:block">
-        <Image src={mango} alt="Mango" className="h-auto w-40" />
-      </div>
+    <div className="font-tsu-sahitya relative min-h-[80vh] w-full">
+      {/* Top Mango */}
+      <Image
+        src={mango}
+        alt="Top Mango"
+        className="absolute top-[65] right-[100] h-auto w-36"
+      />
 
-      <div className="absolute top-70 left-[-15] hidden -translate-x-1/2 2xl:block">
+      {/* Left Mango */}
+      <div className="absolute top-[245] left-[110] hidden 2xl:block">
         <Image src={mango2} alt="Mango 2" className="h-auto w-36" />
       </div>
-      <div className="text-center">
-        <div className="mb-15 flex items-center justify-center gap-3 md:mb-10 lg:justify-start">
+
+      {/* Main Content */}
+      <div className="border-tsu-blue-100 mx-auto w-[85vw] border-b-8 py-20 pb-[150px]">
+        <div className="mb-10 flex items-baseline gap-3">
           <div className="border-tsu-orange-100 h-10 border-l-8" />
           <p className="text-4xl md:text-5xl">Why Join Us?</p>
         </div>
-        <p className="text-lg lg:text-left lg:text-2xl">
+        <p className="text-lg lg:text-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -29,14 +33,7 @@ const WhyJoinUs = () => {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
-
-      <div className="mt-15 mb-7 flex justify-center gap-3 2xl:hidden">
-        <Image src={mango3} alt="Mango 3" className="h-auto w-20" />
-        <Image src={mango3} alt="Mango 3" className="h-auto w-20" />
-        <Image src={mango3} alt="Mango 3" className="h-auto w-20" />
-      </div>
     </div>
   );
 };
-
 export default WhyJoinUs;
