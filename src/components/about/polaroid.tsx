882927eec63b2eb25e2cd4d2160460ec64610364
Hannah Hwang <hannahhwang@mac.mynetworksettings.com>
2025-08-20 20:03:38 -0700

    responsiveness

--- src/components/about/polaroid.tsx
+++ src/components/about/polaroid.tsx
@@ -11,13 +11,13 @@ const ContainerAnimation = {
   transition: { duration: 0.8, staggerChildren: 0.3 },
 };
 const PolaroidAnimation1 = {
-  initial: { opacity: 0, x: 50, rotate: -15 },
-  whileInView: { opacity: 1, x: 150, rotate: -5 },
+  initial: { opacity: 0, x: 0, rotate: -15 },
+  whileInView: { opacity: 1, x: 10, rotate: -5 },
   transition: { duration: 0.8, ease: easeInOut },
 };
 const PolaroidAnimation2 = {
-  initial: { opacity: 0, x: 150, rotate: 15 },
-  whileInView: { opacity: 1, x: 50, rotate: 5 },
+  initial: { opacity: 0, x: 0, rotate: 15 },
+  whileInView: { opacity: 1, x: -5, rotate: 5 },
   transition: { duration: 0.8, ease: easeInOut },
 };
 
@@ -25,20 +25,20 @@ const Polaroid = () => {
   return (
     <motion.div
       {...ContainerAnimation}
-      className="my-6 flex flex-col items-center justify-center gap-6 p-4 md:my-12 md:flex-row md:p-12"
+      className="flex flex-col items-center justify-center gap-10 overflow-hidden p-4 md:flex-row md:gap-20 md:p-15"
     >
       <motion.div {...PolaroidAnimation1}>
         <Image
           src={polaroid}
           alt="Polaroid 1 Image"
-          className="w-full md:-mt-6 md:w-3/5"
+          className="mx-auto w-4/5 md:-mt-6 md:w-2xl xl:w-4/5"
         />
       </motion.div>
       <motion.div {...PolaroidAnimation2}>
         <Image
           src={polaroid2}
           alt="Polaroid 2 Image"
-          className="w-full md:-mt-6 md:w-3/5"
+          className="mx-auto w-4/5 md:-mt-6 md:w-2xl xl:w-4/5"
         />
       </motion.div>
     </motion.div>
