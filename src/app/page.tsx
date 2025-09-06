ce23f78dfb7dc45b720da020f9e527e07a3c5df8
Hannah Hwang <hannahhwang@mac.mynetworksettings.com>
2025-09-06 15:55:02 -0700

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import ThaiStudentUnion from "@/components/home/ThaiStudentUnion";
 import WhoAreWe from "@/components/home/whoAreWe";
 import WhyJoinUs from "@/components/home/whyJoinUs";
-import LoyKrathong from "@/components/home/LoyKrathong";
 import Carousel from "@/components/home/Carousel";
 
 export default function Home() {
